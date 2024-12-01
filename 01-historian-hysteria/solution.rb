3c0bef438c85081efc0de112535f543f8886d439
Paolo <paolobasa@gmail.com>
2024-12-01 15:47:17 +0800

    Add labels to output

--- 01-historian-hysteria/solution.rb
+++ 01-historian-hysteria/solution.rb
@@ -1,6 +1,5 @@
 def load_input
-  in_1 = []
-  in_2 = []
+  in_1, in_2 = [], []
 
   File.open('./input.txt').each do |row|
     a, b = row.split
@@ -8,10 +7,7 @@ def load_input
     in_2 << b.to_i
   end
 
-  in_1.sort!
-  in_2.sort!
-  
-  return in_1, in_2
+  [in_1.sort!, in_2.sort!]
 end
 
 def total_distance(in_1, in_2)
@@ -22,15 +18,11 @@ def total_distance(in_1, in_2)
 end
 
 def similarity_score(in_1, in_2)
-  in_2_tally = in_2.tally
-  score = 0
-
+  in_2_tally, score = in_2.tally, 0
   in_1.each {|v| score += in_2_tally[v].nil? ? 0 : v * in_2_tally[v]}
 
   score
 end
 
 in_1, in_2 = load_input
-print total_distance(in_1, in_2)
-print "\n"
-print similarity_score(in_1, in_2)
+puts 'total_distance = %d, similarity_score = %d' % [total_distance(in_1, in_2), similarity_score(in_1, in_2)]
